eb4a017eb87ba08b9e37ebd8729d2affa4842336
박준수 <pjsu94@naver.com>
2021-10-18 13:55:08 +0900

    COMMENT : 기존 라우터 삭제 및 메인 페이지 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Main from '../views/Main.vue'
+
 import Login from '../views/Login.vue'
 import Singup from '../views/Singup.vue'
 import Profile from '../views/Profile.vue'
-import Navbar from '../views/Navbar.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name : Main,
+    component : Main,
+
   },
   {
     path: '/login',
@@ -34,11 +27,7 @@ const routes = [
     name: 'Profile',
     component: Profile
   },
-  {
-    path: '/navbar',
-    name : Navbar,
-    component : Navbar
-  }
+
 ]
 
 const router = createRouter({
