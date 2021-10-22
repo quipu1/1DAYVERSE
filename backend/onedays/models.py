b161a3a14149d0a21e0d4d04a420e2694ad1d2c5
성루비 <quipu1@naver.com>
2021-10-22 10:49:11 +0900

    feat: 모델 구축
    21.10.22 accounts와 onedays의 db 모델 구축
    
    #S05P31C202-49

--- backend/onedays/models.py
+++ backend/onedays/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from accounts.models import Tutor, Tutee
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+class Lecture(models.Model):
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    tutee = models.ManyToManyField(Tutee, through='Registration')
+    name = models.CharField(max_length=15)
+    main_image = models.ImageField(upload_to='images/', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    password = models.CharField(max_length=30)
+    time = models.CharField(max_length=150)
+    description = models.TextField()
+    room_size = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(3)])
+    price = models.PositiveIntegerField()
+    lecture_cnt = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(7)])
+    validation = models.BooleanField(default=False)
+
+
+class Registration(models.Model):
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+    tutee = models.ForeignKey(Tutee, on_delete=models.CASCADE)
+
+class Attendance(models.Model):
+    registration = models.ForeignKey(Registration, on_delete=models.CASCADE)
+    arrive_at = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(default=0)
+
