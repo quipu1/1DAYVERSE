6687b557b3ddb139a33777cd6e3e53f1c266d91a
박준수 <pjsu94@naver.com>
2021-10-27 15:28:45 +0900

    FEAT: 푸터 추가

--- frontend/src/views/BuyCancel.vue
+++ frontend/src/views/BuyCancel.vue
@@ -4,16 +4,19 @@
     <main>
       <h1>구매가 취소되었습니다.</h1>
     </main>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name : "BuyCancel",
   components : {
     Navbar,
+    Footer,
   },
   data(){
     return {
--- frontend/src/views/LectureCreate.vue
+++ frontend/src/views/LectureCreate.vue
@@ -139,16 +139,19 @@
         </div>
       </div>
     </main>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name : "ClassCreate",
   components : {
     Navbar,
+    Footer,
   },
   data(){
     return {
