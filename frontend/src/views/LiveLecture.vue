5f83336bd7df8d33f5f768bf429cc4b7fb593584
윤소영 <xy1216@naver.com>
2021-10-28 14:54:10 +0900

    Test webrtc : Update 페이지 테스트 문구 추가

---
+++ frontend/src/views/LiveLecture.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="LiveRoot">
+    <div class="content-box">
+      <h1> TEST </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
