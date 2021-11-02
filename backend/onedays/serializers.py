83f9e1c7e5e26b8c2d03429dfe0b0101838c0d73
박준수 <pjsu94@naver.com>
2021-11-02 01:14:51 +0000

    Revert "Merge branch 'smallclass' into 'develop'"
    This reverts merge request !92

--- backend/onedays/serializers.py
+++ backend/onedays/serializers.py
@@ -1,14 +1,11 @@
 from django.db import models
 from django.db.models import fields
-from django.db.models.fields.related import RelatedField
 from rest_framework import serializers
-
 from rest_framework.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 import re
 
 from .models import Lecture, Registration
-from accounts.serializers import ProfileSerializer 
 
 class LectureSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,12 +21,10 @@ class LectureListSerializer(serializers.ModelSerializer):
 
 
 class LectureDetailSerializer(serializers.ModelSerializer):
-    profile = serializers.CharField(source=Lecture.tutor_id)
+
     class Meta:
         model = Lecture
-        # fields = ('id', 'title', 'category', 'name', 'description', 'main_image', 'profile')
         exclude = ('password',)
-    
 
 
 class ProfileLectureSerializer(serializers.ModelSerializer):
