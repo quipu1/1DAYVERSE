7bd92d56e371fa9cc18f285f4f919a3f997d1c65
윤소영 <xy1216@naver.com>
2021-11-03 11:08:27 +0900

    Feat webrtcFE : Add lib+Update components

---
+++ frontend/src/components/WebRtc/CameraSetting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="CamSettingRoot">
+    <div class="content-box">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
