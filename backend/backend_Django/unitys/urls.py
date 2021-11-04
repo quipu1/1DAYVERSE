b66fefb6b4909bc491ccff59441b44658833a352
성루비 <quipu1@naver.com>
2021-11-04 16:16:57 +0900

    feat: 캐릭터 설정
    21.11.04 django-unity POST 요청을 통한 유저 캐릭터 설정 저장
    
    #S05P31C202-118

--- backend/backend_Django/unitys/urls.py
+++ backend/backend_Django/unitys/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('info/', views.user_info),
+    path('character/', views.set_character),
 ]
--- backend/backend_Django/unitys/views.py
+++ backend/backend_Django/unitys/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 
-from accounts.models import Tutee, Tutor, User
+from accounts.models import Tutee, Tutor, User, Character
 from accounts.serializers import ProfileSerializer, UserSerializer, ProfileModifySerializer
 from onedays.models import Lecture
 from onedays.serializers import ProfileLectureSerializer
@@ -16,13 +16,31 @@ from onedays.serializers import ProfileLectureSerializer
 
 # Create your views here.
 @api_view(['POST'])
-def user_info(request):
+def set_character(request):
+    get_data = request.data
+
+    # 유니티에서 받아온 데이터
+    username = get_data['nickname']
+    character = get_data['character']
+
+
     data = {
-        'request': request.data,
-        'hi': 'hi'
+        'character': get_data['character'],
     }
-    print(data)
-    print('djangodjangodjangoadjango')
-    print('dkdkdkdkdkdkk')
+
+    # 해당 유저에 데이터 저장
+    user = get_object_or_404(User, username=username)
+    user.character_id = request.POST.get('character')
+    user.save()
+
     return Response(data)
+
+
+# 유니티 캐릭터 등록
+# @api_view(['POST'])
+# def set_character(request):
+#     data = request.data
+
+
+#     return Response('hi')
 
