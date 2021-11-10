4c85d1c8d5ac6db64fa4178cc512ffccbdf9c5ee
성루비 <quipu1@naver.com>
2021-11-10 11:28:23 +0900

    feat: Unity 유저 정보, 강의 정보
    21.11.10 Unity에 유저에 대한 정보와 유저가 수강하는 강의에 대한 정보 보내줌
    
    #S05P31C202-131

--- backend/backend_Django/unitys/urls.py
+++ backend/backend_Django/unitys/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('profile/<str:username>/', views.profile),
+    path('lecture/<str:username>/', views.lecture),
     path('character/', views.set_character),
 ]
