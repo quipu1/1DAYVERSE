5a11784eb6b23a75e1aa1fee2be00bd8367b6650
박준수 <pjsu94@naver.com>
2021-11-16 10:57:50 +0900

    FIX : photon 수정

--- unity/OneDayverse/Assets/Scripts/CateEnterCategoryScene.cs
+++ unity/OneDayverse/Assets/Scripts/CateEnterCategoryScene.cs
@@ -24,30 +24,36 @@ public class CateEnterCategoryScene : MonoBehaviour
 
                     // PhotonNetwork.JoinOrCreateRoom("Exercise", RO, TypedLobby.Default);
                     // PhotonNetwork.LoadLevel(2);
-                    // PhotonNetwork.JoinRoom("Exercise");
+                    PhotonNetwork.JoinRoom("Exercise");
 
-                    PhotonNetwork.JoinOrCreateRoom("Exercise", RO, TypedLobby.Default);
-                    PhotonNetwork.LoadLevel(2);
+                    // PhotonNetwork.JoinOrCreateRoom("Exercise", RO, TypedLobby.Default);
+                    // PhotonNetwork.LoadLevel(2);
                 } else if (hit.transform.gameObject.name == "Category2")
                 {
                     // SceneManager.LoadScene("CateArt");
                     // PhotonNetwork.JoinOrCreateRoom("Exercise", RO, TypedLobby.Default);
                     // PhotonNetwork.JoinRoom("Art");
-                    PhotonNetwork.JoinOrCreateRoom("Art", RO, TypedLobby.Default);
-                    PhotonNetwork.LoadLevel(3);
+                    // PhotonNetwork.JoinOrCreateRoom("Art", RO, TypedLobby.Default);
+                    // PhotonNetwork.LoadLevel(3);
+                    PhotonNetwork.JoinRoom("Art");
                 }
                 else if (hit.transform.gameObject.name == "Category3")
                 {
                     // SceneManager.LoadScene("CateLanguage");
 
                     // PhotonNetwork.JoinOrCreateRoom("Language", RO, TypedLobby.Default);
-                    PhotonNetwork.JoinOrCreateRoom("Language", RO, TypedLobby.Default);
-                    PhotonNetwork.LoadLevel(4);
+                    // PhotonNetwork.JoinOrCreateRoom("Language", RO, TypedLobby.Default);
+                    // PhotonNetwork.LoadLevel(4);
+                    PhotonNetwork.JoinRoom("Language");
+
                 }               
                 else if (hit.transform.gameObject.name=="Fountain")
                 {
+                    // PhotonNetwork.JoinOrCreateRoom("Lounge", RO, TypedLobby.Default);
                     PhotonNetwork.JoinOrCreateRoom("Lounge", RO, TypedLobby.Default);
-                    PhotonNetwork.LoadLevel(6);
+                    // PhotonNetwork.JoinRoom("Lounge");
+
+                    // PhotonNetwork.LoadLevel(6);
 
                 }
 
