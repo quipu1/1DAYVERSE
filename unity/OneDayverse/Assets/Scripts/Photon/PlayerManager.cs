6c74e19f404aeb1a6ba8b1db5c23b8bace2af0a5
이혜은 <snflo16@naver.com>
2021-11-17 00:45:16 +0900

    Update multi-player

--- unity/OneDayverse/Assets/Scripts/Photon/PlayerManager.cs
+++ unity/OneDayverse/Assets/Scripts/Photon/PlayerManager.cs
@@ -2,12 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System;
 using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine.Networking;
 
 public class PlayerManager : MonoBehaviour
-{   
+{
+    [DllImport("__Internal")]
+    private static extern string GetUsername();
 
     PhotonView PV;
+
+    [Serializable]
+    public class Profile
+    {
+        public string id;
+        public string username;
+        public string birth_day;
+        public string phone_number;
+        public int character;
+        public string profile_image;
+        public string teachable;
+    }
+
+    public int charnum;
+    public string currentUsername;
 
     GameObject controller;
     // Start is called before the first frame update
@@ -18,18 +38,38 @@ public class PlayerManager : MonoBehaviour
 
     void Start()
     {
-        if(PV.IsMine)
+        currentUsername = "idididid2";
+        StartCoroutine(GetUserInfo());
+        
+    }
+
+    public IEnumerator GetUserInfo()
+    {
+        UnityWebRequest userInforequest;
+        using (userInforequest = UnityWebRequest.Get("https://k5c202.p.ssafy.io/od/unitys/profile/" + "oxoxo" + "/"))
+        {
+            yield return userInforequest.SendWebRequest();
+
+            Profile profileJson = JsonUtility.FromJson<Profile>(userInforequest.downloadHandler.text);
+            print(userInforequest.downloadHandler.text);
+            charnum = profileJson.character;
+        }
+        if (PV.IsMine)
         {
             CreateController();
         }
     }
     // Update is called once per frame
     void CreateController()
-    {       
+    {   
+
         // Debug.Log("플레이어 컨트롤러 생성");
         Transform spawnpoint =SpawnManager.Instance.GetSpawnpoint(4);
         // PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
-        controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnpoint.position,  spawnpoint.rotation, 0, new object[] {PV.ViewID});
+        print("캐릭터 번호");
+        print(charnum);
+        
+        controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", charnum.ToString()), spawnpoint.position,  spawnpoint.rotation, 0, new object[] {PV.ViewID});
 
     }
 
